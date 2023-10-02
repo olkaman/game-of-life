f818377cd4f80892e2721433538fc8dcad9c64d3
Aleksandra Manczyk <aleksandra.manczyk@sienn.pl>
2023-10-02 20:41:13 +0200

    Set random cells

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.scss';
 import Square from './components/Square/Square';
 
 const rowCount = 30;
-const colCount = 20;
+const colCount = 30;
 const createGrid = () => {
   const row = [];
   for (let i = 0; i < rowCount; i++) {
@@ -22,15 +22,29 @@ function App() {
     setGrid(newArray);
   };
 
+  const setRandomCells = () => {
+    const newArray = [...grid];
+    newArray.forEach((row) => {
+      row.forEach((col, colIndex) => {
+        const randomNumber = Math.random() < 0.7 ? 0 : 1;
+        row[colIndex] = randomNumber;
+      });
+    });
+    setGrid(newArray);
+  };
+
   return (
-    <div style={{ display: 'grid', gridTemplateColumns: `repeat(${colCount}, 20px)` }}>
-      {grid.map((row, rowIndex) =>
-        row.map((col, colIndex) => {
-          const isAlive = grid[rowIndex][colIndex] === 1;
-          return <Square key={rowIndex + colIndex} setCellState={() => setCellState(rowIndex, colIndex)} isAlive={isAlive} />;
-        })
-      )}
-    </div>
+    <>
+      <div style={{ display: 'grid', gridTemplateColumns: `repeat(${colCount}, 20px)`, gridTemplateRows: `repeat(${rowCount}, 20px)` }}>
+        {grid.map((row, rowIndex) =>
+          row.map((col, colIndex) => {
+            const isAlive = grid[rowIndex][colIndex] === 1;
+            return <Square key={rowIndex + colIndex} setCellState={() => setCellState(rowIndex, colIndex)} isAlive={isAlive} />;
+          })
+        )}
+      </div>
+      <button onClick={setRandomCells}>Set random cells</button>
+    </>
   );
 }
 
