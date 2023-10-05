fc5b9742d217bc0db36c3f73a75d2aeac2acdacb
Aleksandra Manczyk <aleksandra.manczyk@sienn.pl>
2023-10-05 22:34:06 +0200

    Refactor code for proper working game

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './App.scss';
 import Square from './components/Square/Square';
 
-const rowCount = 30;
-const colCount = 30;
+const rowCount = 20;
+const colCount = 20;
 const createGrid = () => {
   const row = [];
   for (let i = 0; i < rowCount; i++) {
@@ -14,24 +14,28 @@ const createGrid = () => {
 
 function App() {
   const [grid, setGrid] = useState(createGrid());
+  const [gameIsOn, setGameIsOn] = useState(false);
+  const previousGrid = useRef();
+
+  useEffect(() => {
+    previousGrid.current = grid;
+  }, [grid]);
 
   const setCellState = (x, y) => {
-    const newArray = [...grid];
-    newArray[x][y] = 1;
-    console.log('new', newArray);
-    setGrid(newArray);
-    findNeighborsOfGivenCell([x, y]);
+    const newGrid = [...grid];
+    newGrid[x][y] = newGrid[x][y] === 0 ? 1 : 0;
+    setGrid(newGrid);
   };
 
   const setRandomCells = () => {
-    const newArray = [...grid];
-    newArray.forEach((row) => {
+    const newGrid = [...grid];
+    newGrid.forEach((row, rowIndex) => {
       row.forEach((col, colIndex) => {
         const randomNumber = Math.random() < 0.7 ? 0 : 1;
-        row[colIndex] = randomNumber;
+        newGrid[rowIndex][colIndex] = randomNumber;
       });
     });
-    setGrid(newArray);
+    setGrid(newGrid);
   };
 
   const possibleNeighbours = [
@@ -46,18 +50,38 @@ function App() {
   ];
 
   const findNeighborsOfGivenCell = ([x, y]) => {
-    console.log([x, y]);
     let neighborsCount = 0;
     possibleNeighbours.forEach((neighbor) => {
       const newX = x + neighbor[0];
       const newY = y + neighbor[1];
       if (newX >= 0 && newX < rowCount && newY >= 0 && newY < colCount) {
-        const isNeighbor = grid[newX][newY] === 1;
+        const isNeighbor = previousGrid.current[newX][newY] === 1;
         isNeighbor && neighborsCount++;
       }
     });
-    console.log('neighborsCount', neighborsCount);
+
     return neighborsCount;
+  };
+
+  const startLifeGame = () => {
+    const newGrid = JSON.parse(JSON.stringify(previousGrid.current));
+    previousGrid.current.forEach((row, rowIndex) => {
+      row.forEach((cell, colIndex) => {
+        const neighbors = findNeighborsOfGivenCell([rowIndex, colIndex]);
+        console.log('neighbors', neighbors, rowIndex, colIndex);
+        if (neighbors < 2 || neighbors > 3) {
+          newGrid[rowIndex][colIndex] = 0;
+        }
+        if (previousGrid.current[rowIndex][colIndex] === 0 && neighbors === 3) {
+          newGrid[rowIndex][colIndex] = 1;
+        }
+      });
+    });
+
+    setGrid(newGrid);
+    setTimeout(() => {
+      startLifeGame();
+    }, 400);
   };
 
   return (
@@ -71,6 +95,8 @@ function App() {
         )}
       </div>
       <button onClick={setRandomCells}>Set random cells</button>
+      <button onClick={startLifeGame}>{gameIsOn ? 'Stop game' : 'Make cells live!'}</button>
+      <button onClick={() => setGrid(createGrid)}>Clear board</button>
     </>
   );
 }
