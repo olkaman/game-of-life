137dc966efa6c8de4b2279ca3f5b38fab14b776c
Aleksandra Manczyk <aleksandra.manczyk@sienn.pl>
2023-10-05 22:34:15 +0200

    Add styling

--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -2,7 +2,7 @@ import styles from './Square.module.scss';
 import clsx from 'clsx';
 
 function Square({ setCellState, isAlive }) {
-  return <button type='button' onClick={setCellState} className={clsx(styles.square, isAlive && styles.alive)}></button>;
+  return <button type='button' onClick={setCellState} className={clsx(styles.square, isAlive ? styles.alive : styles.dead)}></button>;
 }
 
 export default Square;
