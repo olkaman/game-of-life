1ebb76c4251a448b5913e492789c0d8188e004d3
Aleksandra Manczyk <aleksandra.manczyk@sienn.pl>
2023-11-09 20:19:26 +0100

    clean up file with figures

--- src/utils/figures.js
+++ src/utils/figures.js
@@ -1,35 +1,104 @@
-export const square = [
-  [1, 1],
-  [1, 1],
-];
+import squareLink from '../assets/square.svg';
+import beeHiveLink from '../assets/bee-hive.svg';
+import tubLink from '../assets/tub.svg';
+import loafLink from '../assets/loaf.svg';
+import toadLink from '../assets/toad.svg';
+import blinkerLink from '../assets/blinker.svg';
+import penthadecathlonLink from '../assets/penthadecathlon.svg';
+import pulsarLink from '../assets/pulsar.svg';
+import gliderLink from '../assets/glider.svg';
+import spaceshipSLink from '../assets/spaceship1.svg';
+import spaceshipMLink from '../assets/spaceship2.svg';
+import spaceshipLLink from '../assets/spaceship3.svg';
+import lineLink from '../assets/line.svg';
 
-export const toad = [
-  [0, 0, 0, 0],
-  [1, 1, 1, 0],
-  [0, 1, 1, 1],
-  [0, 0, 0, 0],
-];
+const square = {
+  name: 'Square',
+  link: squareLink,
+  shape: [
+    [1, 1],
+    [1, 1],
+  ],
+};
 
-export const glider = [
-  [0, 1, 0],
-  [0, 0, 1],
-  [1, 1, 1],
-];
+const tub = {
+  name: 'Tub',
+  link: tubLink,
+  shape: [
+    [0, 1, 0],
+    [1, 0, 1],
+    [0, 1, 0],
+  ],
+};
 
-export const pulsar = [
-  [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
-  [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0],
-  [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0],
-];
+const beeHive = {
+  name: 'Bee hive',
+  link: beeHiveLink,
+  shape: [
+    [0, 1, 1, 0],
+    [1, 0, 0, 1],
+    [0, 1, 1, 0],
+  ],
+};
 
-export const line = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]];
+const blinker = {
+  name: 'Blinker',
+  link: blinkerLink,
+  shape: [[1], [1], [1]],
+};
+
+const penthadecathlon = {
+  name: 'Penthadecathlon',
+  link: penthadecathlonLink,
+  shape: [],
+};
+
+const toad = {
+  name: 'Toad',
+  link: toadLink,
+  shape: [
+    [0, 0, 0, 0],
+    [1, 1, 1, 0],
+    [0, 1, 1, 1],
+    [0, 0, 0, 0],
+  ],
+};
+
+const glider = {
+  name: 'Glider',
+  link: gliderLink,
+  shape: [
+    [0, 1, 0],
+    [0, 0, 1],
+    [1, 1, 1],
+  ],
+};
+
+const pulsar = {
+  name: 'Pulsar',
+  link: pulsarLink,
+  shape: [
+    [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+    [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0],
+    [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0],
+  ],
+};
+
+const line = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]];
+
+export const allFigures = {
+  stillLives: [square, tub, beeHive],
+  oscillators: [blinker, toad, pulsar],
+  spaceships: [glider],
+  lines: [],
+};
