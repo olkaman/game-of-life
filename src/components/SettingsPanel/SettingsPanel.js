545761021f15f1d67e00004dc1fd5d7b27792371
Aleksandra Manczyk <aleksandra.manczyk@sienn.pl>
2023-11-10 20:55:18 +0100

    More styling

--- src/components/SettingsPanel/SettingsPanel.js
+++ src/components/SettingsPanel/SettingsPanel.js
@@ -1,19 +1,15 @@
 import { useEffect, useState, useRef } from 'react';
 import Button from '../Button/Button';
-import { rowCount, colCount } from '../../utils/boardSize';
-import { useCreateGrid } from '../../hooks/useCreateGrid';
-import { possibleNeighbors } from '../../utils/possibleNeighbors';
 import styles from './SettingsPanel.module.scss';
 import { useGameStore } from '../../store/store';
 import AddFigures from '../AddFigures/AddFigures';
 import Player from '../Player/Player';
+import clsx from 'clsx';
 
 function SettingsPanel() {
   const previousGrid = useRef();
   const previousCounter = useRef();
-  const [timerId, setTimerId] = useState();
   const [counter, setCounter] = useState(0);
-  const createGrid = useCreateGrid();
   const grid = useGameStore((state) => state.grid);
   const setGrid = useGameStore((state) => state.setGrid);
 
@@ -21,34 +17,6 @@ function SettingsPanel() {
     previousGrid.current = grid;
     previousCounter.current = counter;
   }, [grid, counter]);
-
-  const playLifeGame = () => {
-    // TODO grid does noti include 1 return
-    setCounter(previousCounter.current + 1);
-    const newGrid = previousGrid.current.map((arr) => {
-      return arr.slice();
-    });
-    previousGrid.current.forEach((row, rowIndex) => {
-      row.forEach((cell, colIndex) => {
-        const neighbors = findNeighborsOfGivenCell([rowIndex, colIndex]);
-
-        if (neighbors < 2 || neighbors > 3) {
-          newGrid[rowIndex][colIndex] = 0;
-        }
-        if (previousGrid.current[rowIndex][colIndex] === 0 && neighbors === 3) {
-          newGrid[rowIndex][colIndex] = 1;
-        }
-      });
-    });
-
-    setGrid(newGrid);
-
-    const timerId = setTimeout(() => {
-      playLifeGame();
-    }, 400);
-
-    setTimerId(timerId);
-  };
 
   const setRandomCells = () => {
     const newGrid = [...grid];
@@ -61,43 +29,31 @@ function SettingsPanel() {
     setGrid(newGrid);
   };
 
-  const findNeighborsOfGivenCell = ([x, y]) => {
-    let neighborsCount = 0;
-    possibleNeighbors.forEach((neighbor) => {
-      const newX = x + neighbor[0];
-      const newY = y + neighbor[1];
-      if (newX >= 0 && newX < rowCount && newY >= 0 && newY < colCount) {
-        const isNeighbor = previousGrid.current[newX][newY] === 1;
-        isNeighbor && neighborsCount++;
-      }
-    });
-
-    return neighborsCount;
-  };
-
-  const pauseGame = () => {
-    clearTimeout(timerId);
-    setTimerId(0);
-  };
-
   return (
     <section className={styles.settingsPanel}>
-      <div className={styles.wrapper}>
-        <h2>Game of life</h2>
-        <p>
-          Select cells manually <br />
-          or select ready figures
-        </p>
-        <p>Number of cycles: {counter}</p>
-        <Button handleClick={setRandomCells} type='primary'>
-          Set random cells
-        </Button>
-        <AddFigures />
-        <Player />
-        Rules of{' '}
-        <a href='https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life' target='blank'>
-          Conway's Game of life
-        </a>
+      <div className={clsx(styles.wrapper, 'flex')}>
+        <div>
+          <section>
+            <h2>Game of life</h2>
+            <p>
+              Select cells manually <br />
+              or select ready figures
+            </p>
+            <p>Number of cycles: {counter}</p>
+          </section>
+          <Player />
+          <section className={styles.buttons}>
+            <Button handleClick={setRandomCells}>Set random cells</Button>
+            <AddFigures />
+          </section>
+        </div>
+
+        <aside>
+          Rules of{' '}
+          <a href='https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life' target='blank'>
+            Conway's Game of life
+          </a>
+        </aside>
       </div>
     </section>
   );
