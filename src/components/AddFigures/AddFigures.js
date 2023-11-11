a94cf1e6c60b7338f3966b756db99d8d8148724b
Aleksandra Manczyk <aleksandra.manczyk@sienn.pl>
2023-11-11 22:19:47 +0100

    Style figures panel

--- src/components/AddFigures/AddFigures.js
+++ src/components/AddFigures/AddFigures.js
@@ -1,21 +1,24 @@
-import { useEffect, useState, useRef } from 'react';
+import { useState } from 'react';
 import Button from '../Button/Button';
 import { allFigures } from '../../utils/figures.js';
 import { rowCount, colCount } from '../../utils/boardSize';
-import { useCreateGrid } from '../../hooks/useCreateGrid';
-import { possibleNeighbors } from '../../utils/possibleNeighbors';
 import styles from './AddFigures.module.scss';
 import FigureButton from '../FigureButton/FigureButton';
 import { generateKey } from '../../utils/generateKey';
 import { useGameStore } from '../../store/store';
+import clsx from 'clsx';
 
 export default function AddFigures() {
   const grid = useGameStore((state) => state.grid);
   const setGrid = useGameStore((state) => state.setGrid);
   const [openFigures, setOpenFigures] = useState(false);
+  const [lineInputValue, setLineInputValue] = useState(3);
+  const [line, setLine] = useState([Array(3).fill(1)]);
+  console.log(lineInputValue, line);
 
   function setFigure(figure) {
     setOpenFigures(false);
+    setLineInputValue(3);
     const halfBoardWidth = Math.ceil((colCount - figure[0].length) / 2);
     const halfBoardHeight = Math.ceil((rowCount - figure.length) / 2);
 
@@ -30,31 +33,45 @@ export default function AddFigures() {
 
     setGrid(newGrid);
   }
+
+  const setLineLength = (e) => {
+    setLineInputValue(e.target.value);
+    const line = [Array(Number(e.target.value)).fill(1)];
+    setLine(line);
+  };
+
   return (
     <>
       <Button handleClick={() => setOpenFigures(!openFigures)}>{openFigures ? '-' : '+'} Add figure</Button>
       {openFigures && (
-        <>
-          <p>Still lives</p>
-          <section className='flex'>
+        <section className={styles.wrapper}>
+          <h5>Still lives</h5>
+          <section className={clsx(styles.picturesWrapper, 'flex')}>
             {allFigures.stillLives.map((figure) => {
               return <FigureButton key={generateKey(figure.name)} figureImg={figure.link} handleClick={() => setFigure(figure.shape)} figureName={figure.name} />;
             })}
           </section>
-          <p>Oscillators</p>
-          <section className='flex'>
+          <h5>Oscillators</h5>
+          <section className={clsx(styles.picturesWrapper, 'flex')}>
             {allFigures.oscillators.map((figure) => {
               return <FigureButton key={generateKey(figure.name)} figureImg={figure.link} handleClick={() => setFigure(figure.shape)} figureName={figure.name} />;
             })}
           </section>
-          <p>Spaceships</p>
-          <section className='flex'>
+          <h5>Spaceships</h5>
+          <section className={clsx(styles.picturesWrapper, 'flex')}>
             {allFigures.spaceships.map((figure) => {
               return <FigureButton key={generateKey(figure.name)} figureImg={figure.link} handleClick={() => setFigure(figure.shape)} figureName={figure.name} />;
             })}
           </section>
-          <p>Lines</p>
-        </>
+          <h5>Lines</h5>
+          <div className={clsx(styles.lineWrapper, 'flex')}>
+            {' '}
+            <input value={lineInputValue} onChange={setLineLength} placeholder='Enter line length' type='number' min='3' max={colCount} />
+            <Button handleClick={() => setFigure(line)} type='primary'>
+              Create
+            </Button>
+          </div>
+        </section>
       )}
     </>
   );
