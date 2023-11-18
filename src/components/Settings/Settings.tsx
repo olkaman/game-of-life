9fc41455a615f7ba4037f4d503fdf14f4bf1c444
Aleksandra Manczyk <aleksandra.manczyk@sienn.pl>
2023-11-18 18:50:36 +0100

    Remove setting cells size

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -2,19 +2,16 @@ import { useState } from 'react';
 import Button from '../Button/Button';
 import styles from './Settings.module.scss';
 import { useGameStore } from '../../store/store';
-import { minCellSize, maxCellSize, minSpeed, maxSpeed } from '../../utils/settings';
+import { minSpeed, maxSpeed } from '../../utils/settings';
 
 export default function Settings() {
   const [areSettingsOpen, setAreSettingsOpen] = useState<boolean>(false);
-  const [cellsSize, setCellsSize] = useState<string>('20');
   const [gameSpeed, setGameSpeed] = useState<string>('400');
   const setSpeed = useGameStore((state) => state.setSpeed);
-  const setCellSize = useGameStore((state) => state.setCellSize);
 
   const handleSettingsChange = (e: any) => {
     e.preventDefault();
     setSpeed(Number(gameSpeed));
-    setCellSize(Number(cellsSize));
     setAreSettingsOpen(false);
   };
 
@@ -26,14 +23,10 @@ export default function Settings() {
       {areSettingsOpen && (
         <form className={styles.fieldsWrapper} onSubmit={handleSettingsChange}>
           <div className={styles.input}>
-            <label htmlFor='cellSize'>{`Cell size (values: ${minCellSize}-${maxCellSize} px)`}</label>
-            <input name='cellSize' id='cellSize' value={cellsSize} onChange={(e) => setCellsSize(e.target.value)} type='number' min={minCellSize} max={maxCellSize} />
-          </div>
-          <div className={styles.input}>
             <label htmlFor='speed'>{`Speed (values: ${minSpeed}-${maxSpeed} miliseconds)`}</label>
             <input name='speed' id='speed' value={gameSpeed} onChange={(e) => setGameSpeed(e.target.value)} type='number' min={minSpeed} max={maxSpeed} />
           </div>
-          <Button type='submit' disabled={Number(cellsSize) < minCellSize || Number(cellsSize) > maxCellSize || Number(gameSpeed) < minSpeed || Number(gameSpeed) > maxSpeed}>
+          <Button type='submit' disabled={Number(gameSpeed) < minSpeed || Number(gameSpeed) > maxSpeed}>
             Save
           </Button>
         </form>
--- src/store/store.ts
+++ src/store/store.ts
@@ -6,19 +6,16 @@ type StateType = State | Partial<State> | ((state: State) => State | Partial<Sta
 type State = {
   grid: number[][];
   speed: number;
-  squareSize: number;
 };
 
 type Actions = {
   setGrid: (newGrid: number[][]) => void;
   setSpeed: (speed: number) => void;
-  setCellSize: (squareSize: number) => void;
 };
 
 const initialState: State = {
   grid: [[]],
   speed: 400,
-  squareSize: 20,
 };
 
 //
@@ -27,7 +24,6 @@ export const useGameStore = create<State & Actions, [['zustand/devtools', State 
     ...initialState,
     setGrid: (newGrid: number[][]) => set(setGrid(newGrid), false, 'Set grid'),
     setSpeed: (speed: number) => set(setSpeed(speed), false, 'Set speed'),
-    setCellSize: (squareSize: number) => set(setCellSize(squareSize), false, 'Set speed'),
   }))
 );
 
@@ -40,11 +36,5 @@ function setGrid(newGrid: number[][]): StateType {
 function setSpeed(speed: number): StateType {
   return (state) => {
     return { ...state, speed };
-  };
-}
-
-function setCellSize(squareSize: number): StateType {
-  return (state) => {
-    return { ...state, squareSize };
   };
 }
